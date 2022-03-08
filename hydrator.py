d0633e2ea689e50c5bb73b5b6012d57aa5d4e196
Binula Kavisinghe <81571678+Binula-Kavisinghe@users.noreply.github.com>
2022-03-08 06:54:30 +0530

    Update hydrator.py

--- hydrator.py
+++ hydrator.py
@@ -1,13 +1,14 @@
 import time
 
+#get current system time
 t = time.localtime()
 current_time = time.strftime("%H:%M:%S", t)
 print(current_time)
 
-current_hour = int(current_time [0:2])
+current_hour = int(current_time [0:2]) #get the current hour
 print("current_hour - ", current_hour)
 
-start_hour = 5.5
+start_hour = 8
 increment = 1
 
 current_dif = current_hour - start_hour
